08713f7670715d4de6ea9455b27e96267874f99c
MaryDias <166772912+LabDevOps09@users.noreply.github.com>
2024-09-30 16:08:36 -0300

    Update desafio.py
    Adicionando código do desafio de projeto

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -1,5 +1,4 @@
 menu = """
-
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -23,7 +22,7 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
-
+            print(f"Depósito de R$ {valor:.2f} realizado com sucesso!")
         else:
             print("Operação falhou! O valor informado é inválido.")
 
@@ -31,36 +30,33 @@ while True:
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
-
         excedeu_limite = valor > limite
-
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
             print("Operação falhou! Você não tem saldo suficiente.")
-
         elif excedeu_limite:
             print("Operação falhou! O valor do saque excede o limite.")
-
         elif excedeu_saques:
             print("Operação falhou! Número máximo de saques excedido.")
-
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
-
+            print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
         else:
             print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
+        print(f"Saldo: R$ {saldo:.2f}")
         print("==========================================")
 
     elif opcao == "q":
+        print("Saindo do sistema. Até logo!")
         break
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
+
