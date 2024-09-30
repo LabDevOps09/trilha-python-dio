f9209c9b6336649a5294d6a59aa4bb16692f3134
MaryDias <166772912+LabDevOps09@users.noreply.github.com>
2024-09-30 15:57:10 -0300

    Update desafio.py
    Implementando o código

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -23,6 +23,7 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
+            print(f"Depósito de R$ {valor: .2f} realizado com sucesso!")
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -49,6 +50,7 @@ while True:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
+            print(f"Saque de R$ {valor: .2f} realizado com sucesso!")
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -60,6 +62,7 @@ while True:
         print("==========================================")
 
     elif opcao == "q":
+        print("Saindo do sistema. Até logo!")
         break
 
     else:
